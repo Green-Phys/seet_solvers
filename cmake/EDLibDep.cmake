f5ff5f2df674012b5977f945c5e386d4ab43efc2
Gaurav Harsha <gauravharsha05@gmail.com>
2025-09-18 19:21:14 +0530

    fix typo in git repo url

--- cmake/EDLibDep.cmake
+++ cmake/EDLibDep.cmake
@@ -4,7 +4,7 @@ function(add_edlib_dependency)
 
     FetchContent_Declare(
             EDLib
-            GIT_REPOSITORY https://github.com/gauravharhsa/EDLib.git
+            GIT_REPOSITORY https://github.com/gauravharsha/EDLib.git
             GIT_TAG origin/master # or a later release
             CMAKE_ARGS
                 -DARPACK_ROOT=${ARPACK_DIR}
